f7af4673e6c37f3da4f707d3d121a0abbe77091e
Droidment <droidment@gmail.com>
2019-05-19 22:48:56 -0500

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_analytics/observer.dart';
 import 'package:intl/intl.dart';
 import 'package:scoped_model/scoped_model.dart';
+import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   static FirebaseAnalytics analytics = FirebaseAnalytics();
   static FirebaseAnalyticsObserver observer =
       FirebaseAnalyticsObserver(analytics: analytics);
-
+ 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -31,6 +32,7 @@ class TeamList extends StatefulWidget {
   final String title;
   final FirebaseAnalytics analytics;
   final FirebaseAnalyticsObserver observer;
+
   TeamListState createState() => new TeamListState(analytics, observer);
 }
 
@@ -38,6 +40,17 @@ class TeamListState extends State<TeamList> {
   TeamListState(this.analytics, this.observer);
   final FirebaseAnalyticsObserver observer;
   final FirebaseAnalytics analytics;
+  final Firestore fireStore = Firestore.instance;
+  final _addGameFormKey = GlobalKey<FormState>();
+
+  final formats = {
+    InputType.both: DateFormat("EEE, MMM yyyy dd h:mma"),
+    InputType.date: DateFormat('yy-MM-dd'),
+    InputType.time: DateFormat("HH:mm"),
+  };
+  InputType inputType = InputType.both;
+  bool editable = true;
+  DateTime date;
 
   // final _biggerFont = const TextStyle(fontSize: 18.0);
   @override
@@ -46,7 +59,11 @@ class TeamListState extends State<TeamList> {
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
           onPressed: () {
-            print("FAB Pressed");
+            showDialog(
+                context: context,
+                builder: (_) => new AlertDialog(
+                    title: new Text("Add new game"),
+                    content: _buildAddGame(context)));
           },
         ),
         bottomNavigationBar: BottomAppBar(
@@ -59,10 +76,10 @@ class TeamListState extends State<TeamList> {
                 (BuildContext context, bool innerBoxIsScrolled) {
               return <Widget>[
                 SliverAppBar(
-                  expandedHeight: 100.0,
-                  floating: false,
-                  pinned: true,
-                  flexibleSpace: FlexibleSpaceBar(
+                    expandedHeight: 100.0,
+                    floating: false,
+                    pinned: true,
+                    flexibleSpace: FlexibleSpaceBar(
                       centerTitle: true,
                       title: Text("Katy Whackers",
                           style: TextStyle(
@@ -81,18 +98,73 @@ class TeamListState extends State<TeamList> {
                               ),
                             ],
                           )),
-                      background: Image.asset("assets/volleyball_masthead.jpg",
-                          fit: BoxFit.fill)),
-                ),
+                      background: ClipRRect(
+                          borderRadius: BorderRadius.circular(15.0),
+                          child: Image.asset("assets/volleyball_masthead.jpg",
+                              fit: BoxFit.fill)),
+                    )),
               ];
             },
             body: _buildBody(context)));
     // body: _buildBody(context));
   }
 
+  Widget _buildAddGame(BuildContext context) {
+    TextEditingController fromDateController = TextEditingController();
+    TextEditingController toDateController = TextEditingController();
+    TextEditingController locationController = TextEditingController();
+    return Form(
+      key: _addGameFormKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          DateTimePickerFormField(
+            controller: fromDateController,
+            inputType: inputType,
+            format: formats[inputType],
+            editable: editable,
+            decoration: InputDecoration(
+                labelText: 'From', hasFloatingPlaceholder: false),
+            onChanged: (dt) {},
+          ),
+          DateTimePickerFormField(
+            controller: toDateController,
+            inputType: inputType,
+            format: formats[inputType],
+            editable: editable,
+            decoration:
+                InputDecoration(labelText: 'To', hasFloatingPlaceholder: false),
+            onChanged: (dt) => setState(() => date = dt),
+          ),
+          TextFormField(
+            controller: locationController,
+            decoration: InputDecoration(
+                labelText: 'Location', hasFloatingPlaceholder: true),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 4.0),
+            child: OutlineButton(
+              highlightColor: Colors.amber,
+              onPressed: () async {
+                print(_addGameFormKey.currentState);
+                _addGameFormKey.currentState.save();
+                DateFormat df = new DateFormat("EEE, MMM yyyy dd h:mma");
+                var fromDate = df.parse(fromDateController.text);
+                var toDate = df.parse(fromDateController.text);
+                GameModel gm =  new GameModel(fromDate,toDate, locationController.text);
+                  gm.addGame();
+              },
+              child: Text('Submit'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   Widget _buildBody(BuildContext context) {
     return StreamBuilder<QuerySnapshot>(
-      stream: Firestore.instance.collection('GameModel').snapshots(),
+      stream: fireStore.collection('Game').snapshots(),
       builder: (context, snapshot) {
         if (!snapshot.hasData) return LinearProgressIndicator();
 
@@ -104,12 +176,14 @@ class TeamListState extends State<TeamList> {
   Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
     return ListView(
       padding: const EdgeInsets.only(top: 20.0),
-      children: snapshot.map((data) => _buildListItem(context, data)).toList(),
+      children: snapshot
+          .map((documentSnapshot) => _buildListItem(context, documentSnapshot))
+          .toList(),
     );
   }
 
-  Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
-    final record = GameModel.fromSnapshot(data);
+  Widget _buildListItem(BuildContext context, DocumentSnapshot gameData) {
+    final record = GameModel.fromSnapshot(gameData);
     String dropdownValue = '+0';
     return Padding(
       key: ValueKey(record.captain1),
@@ -119,10 +193,9 @@ class TeamListState extends State<TeamList> {
           child: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
             ListTile(
               title: Text(
-                  new DateFormat.yMMMMEEEEd("en_US").format(record.timeFrom)),
+                  new DateFormat.yMMMMEEEEd("en_US").format(record.timeFrom.toDate())),
               trailing:
                   Text(record.captain1Name + " vs " + record.captain2Name),
-
               subtitle: Text(record.yesCount.toString() +
                   " Yes | " +
                   record.noCount.toString() +
@@ -131,23 +204,18 @@ class TeamListState extends State<TeamList> {
                   " Maybe | " +
                   record.waitlistCount.toString() +
                   " Waitlist"),
-              // : Text("12 Yes   3 No  1 Maybe"),
               onTap: () => print(record),
             ),
             ButtonTheme.bar(
-              // make buttons use the appropriate styles for cards
               child: ButtonBar(
                 children: <Widget>[
                   DropdownButton<String>(
                     value: dropdownValue,
-                    onChanged: (String newValue) {
-                      int guestNum = int.tryParse(newValue) ?? 0;
-                      record.yesCount += guestNum;
-                      print("DropDown Value is " + record.yesCount.toString());
-                      setState(() {
-                        dropdownValue = newValue;
-                      });
-                      
+                    onChanged: (String newValue) async {
+                      int iGuestCount = int.tryParse(newValue) ?? 0;
+                      RSVP rsvpUser = new RSVP(
+                          record.reference, "Raj", "Guest", iGuestCount);
+                      await rsvpUser.doRSVP(rsvpUser, gameData);
                     },
                     items: <String>['+0', '1', '2', '3', '4']
                         .map<DropdownMenuItem<String>>((String value) {
@@ -157,24 +225,37 @@ class TeamListState extends State<TeamList> {
                       );
                     }).toList(),
                   ),
-                  FlatButton(
+                  OutlineButton(
+                    highlightColor: Colors.amber,
                     child: const Text(
                       'Yes',
                       style: TextStyle(color: Colors.teal),
                     ),
-                    onPressed: () {/* ... */},
+                    onPressed: () async {
+                      RSVP rsvpUser = new RSVP(record.reference, "Raj", "Yes");
+                      await rsvpUser.doRSVP(rsvpUser, gameData);
+                    },
                   ),
-                  FlatButton(
+                  OutlineButton(
+                    highlightColor: Colors.amber,
                     textTheme: ButtonTextTheme.normal,
                     child: const Text('No',
                         style: TextStyle(color: Colors.orange)),
-                    onPressed: () {/* ... */},
+                    onPressed: () async {
+                      RSVP rsvpUser = new RSVP(record.reference, "Raj", "No");
+                      await rsvpUser.doRSVP(rsvpUser, gameData);
+                    },
                   ),
-                  FlatButton(
+                  OutlineButton(
+                    highlightColor: Colors.amber,
                     textTheme: ButtonTextTheme.normal,
                     child: const Text('Maybe',
                         style: TextStyle(color: Colors.grey)),
-                    onPressed: () {/* ... */},
+                    onPressed: () async {
+                      RSVP rsvpUser =
+                          new RSVP(record.reference, "Raj", "Maybe", 0);
+                      await rsvpUser.doRSVP(rsvpUser, gameData);
+                    },
                   ),
                 ],
               ),
@@ -203,43 +284,206 @@ class Players {
 }
 
 class RSVP {
-  DateTime timeFrom;
+  DocumentReference game;
+  String playerName;
   String yesNoMaybe;
+  int guestCount;
+  RSVP(DocumentReference game, String playerName, String yesNoMaybe,
+      [int guestCount = 0]) {
+    this.game = game;
+    this.playerName = playerName;
+    this.yesNoMaybe = yesNoMaybe;
+    this.guestCount = guestCount;
+  }
+  toMap() {
+    return {
+      "Game": game,
+      "PlayerName": playerName,
+      "YesNoMaybe": yesNoMaybe,
+      "GuestCount": guestCount
+    };
+  }
+
+  doRSVP(RSVP rsvpUser, DocumentSnapshot gameData) async {
+    //If this user RSVP already exists for this Game, update the response,
+    // If not add.
+    Firestore fireStore = Firestore.instance;
+
+    CollectionReference gamePlayerReference =
+        fireStore.collection("GamePlayers");
+    Query gamePlayerQuery = gamePlayerReference
+        .where("PlayerName", isEqualTo: rsvpUser.playerName)
+        .where("Game", isEqualTo: rsvpUser.game);
+
+    // String sYesNoMayCount = rsvpUser.yesNoMaybe + "Count";
+    int iYesCount = gameData.data["YesCount"];
+    int iNoCount = gameData.data["NoCount"];
+    int iMaybeCount = gameData.data["MaybeCount"];
+    int iGuestCount = gameData.data["GuestCount"];
+    bool bUserPreviouslyHasGuest = false;
+    bool bUserHasGuestNow = false;
+    int iWaitlistCount = gameData.data["WaitlistCount"];
+
+    await gamePlayerQuery.getDocuments().then((results) {
+      if (results.documents.isNotEmpty) {
+        results.documents.forEach((player) {
+          String dbYesNoSelection = player.data['YesNoMaybe'].toString();
+          //If the player had a previous guest selection
+
+          if (rsvpUser.yesNoMaybe != dbYesNoSelection) {
+            // Continue only if user changed the selection
+
+            int playerGuestCount = player.data["GuestCount"];
+            if (playerGuestCount is int && playerGuestCount > 0) {
+              bUserPreviouslyHasGuest = true;
+            }
+            if (rsvpUser.guestCount is int && rsvpUser.guestCount > -1) {
+              iGuestCount = rsvpUser.guestCount;
+              bUserHasGuestNow = true;
+            }
+            if (rsvpUser.yesNoMaybe == "Guest") {
+              rsvpUser.yesNoMaybe = "Yes";
+
+              if (bUserPreviouslyHasGuest) {
+                iYesCount = iYesCount - playerGuestCount;
+              } else {
+                if (player.data["YesNoMaybe"] != "Yes")
+                  iYesCount = iYesCount + 1;
+              }
+              if (bUserHasGuestNow) {
+                iYesCount = iYesCount + iGuestCount;
+                player.data['GuestCount'] = iGuestCount;
+              }
+              if (dbYesNoSelection == "No") {
+                iNoCount = iNoCount - 1;
+              } else if (dbYesNoSelection == "Maybe") {
+                iMaybeCount = iMaybeCount - 1;
+              }
+            } else if (rsvpUser.yesNoMaybe == "Yes") {
+              iYesCount = iYesCount + 1;
+              // if (bUserHasGuestNow) {
+              //   iYesCount = iYesCount + iGuestCount;
+              //   player.data['GuestCount'] = iGuestCount;
+              // }
+              if (dbYesNoSelection == "No") {
+                iNoCount = iNoCount - 1;
+              } else {
+                iMaybeCount = iMaybeCount - 1;
+              }
+            } else if (rsvpUser.yesNoMaybe == "No") {
+              iNoCount = iNoCount + 1;
+              player.data['GuestCount'] = 0;
+              if (dbYesNoSelection == "Yes") {
+                iYesCount = iYesCount - 1;
+                if (bUserPreviouslyHasGuest) {
+                  iYesCount = ((iYesCount - playerGuestCount) > 0)
+                      ? (iYesCount - playerGuestCount)
+                      : 0;
+                }
+              } else {
+                iMaybeCount = iMaybeCount - 1;
+              }
+            } else if (rsvpUser.yesNoMaybe == "Maybe") {
+              iMaybeCount = iMaybeCount + 1;
+              player.data['GuestCount'] = 0;
+              if (dbYesNoSelection == "Yes") {
+                iYesCount = iYesCount - 1;
+                if (bUserPreviouslyHasGuest) {
+                  iYesCount = ((iYesCount - playerGuestCount) > 0)
+                      ? (iYesCount - playerGuestCount)
+                      : 0;
+                }
+              } else {
+                iNoCount = iNoCount - 1;
+              }
+            }
+            player.data['YesNoMaybe'] = rsvpUser.yesNoMaybe;
+            player.reference.updateData(player.data).then((result) {
+              gameData.reference.updateData({
+                "YesCount": iYesCount,
+                "NoCount": iNoCount,
+                "MaybeCount": iMaybeCount,
+                "GuestCount": iGuestCount
+              });
+            });
+          }
+        });
+      } else {
+        if (rsvpUser.yesNoMaybe == "Guest") {
+          rsvpUser.yesNoMaybe = "Yes";
+          iYesCount = iYesCount + 1 + rsvpUser.guestCount;
+        } else if (rsvpUser.yesNoMaybe == "Yes") {
+          iYesCount = iYesCount + 1;
+        } else if (rsvpUser.yesNoMaybe == "No") {
+          iNoCount = iNoCount + 1;
+        } else if (rsvpUser.yesNoMaybe == "Maybe") {
+          iMaybeCount = iMaybeCount + 1;
+        }
+        gamePlayerReference.add(rsvpUser.toMap()).then((result) {
+          gameData.reference.updateData({
+            "YesCount": iYesCount,
+            "NoCount": iNoCount,
+            "MaybeCount": iMaybeCount,
+            "GuestCount": rsvpUser.guestCount
+          });
+        });
+      }
+    });
+  }
 }
 
-class GameModel extends Model{
+class GameModel extends Model {
   DocumentReference captain1;
   DocumentReference captain2;
-  DateTime timeFrom;
-  DateTime timeTo;
+  Timestamp timeFrom;
+  Timestamp timeTo;
   String score;
   String captain1Name;
   String captain2Name;
   String winner;
+  GeoPoint location;
   int yesCount;
   int noCount;
   int maybeCount;
   int waitlistCount;
+  int guestCount;
   DocumentReference reference;
 
+  GameModel(DateTime from, DateTime to, String location){
+    timeFrom = new Timestamp.fromDate(from);
+    timeTo = new Timestamp.fromDate(to);
+    location = location;
+  }
+
   GameModel.fromMap(Map<String, dynamic> map, {this.reference}) {
-    assert(map['Captain1'] != null);
-    assert(map['Captain2'] != null);
+    // assert(map['Captain1'] != null);
+    // assert(map['Captain2'] != null);
     captain1 = map['Captain1'];
     captain2 = map['Captain2'];
     timeFrom = map["TimeFrom"];
+    timeTo = map["TimeTo"];
     captain1Name = map['Captain1Name'];
     captain2Name = map['Captain2Name'];
-    timeTo = map["TimeTo"];
     winner = map["Winner"];
     score = map["Score"];
     yesCount = map["YesCount"];
     noCount = map["NoCount"];
     maybeCount = map["MaybeCount"];
     waitlistCount = map["WaitlistCount"];
+    guestCount = map["GuestCount"];
+    location = map["Location"];
   }
 
-  addRsvp(String yesNoMaybe, int count){
+  addGame() {
+    Firestore fireStore = Firestore.instance;
+    CollectionReference gameReference = fireStore.collection("Game");
+    gameReference
+        .add({"From": timeFrom, "To": timeTo, "Location": location}).then((result) {
+      return result;
+    });
+  }
+
+  addRsvp(String yesNoMaybe, int count) {
     if (yesNoMaybe == 'yes') yesCount = yesCount += count;
     if (yesNoMaybe == 'no') noCount = noCount += count;
     if (yesNoMaybe == 'maybe') maybeCount = maybeCount += count;
