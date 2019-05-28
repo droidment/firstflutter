f4fc0c88d5dce4db4c559b52921cc2cedf50b707
Droidment <droidment@gmail.com>
2019-05-28 18:06:57 -0500

    View Game

--- lib/main.dart
+++ lib/main.dart
@@ -113,64 +113,6 @@ class TeamListState extends State<TeamList> {
     // body: _buildBody(context));
   }
 
-  Widget _viewGame(BuildContext context, GameModel record) {
-    var timeStr = " ";
-    var toDateStr;
-    if (record.timeFrom != null) {
-      toDateStr =
-          new DateFormat.yMMMMEEEEd("en_US").format(record.timeFrom.toDate());
-      var fromTimeStr =
-          new DateFormat.Hm("en_US").format(record.timeFrom.toDate());
-      var toTimeStr = new DateFormat.Hm("en_US").format(record.timeTo.toDate());
-      timeStr = "From " + fromTimeStr + " to " + toTimeStr;
-    }
-    // var fromDateStr = new DateFormat.yMMMMEEEEd("en_US").format(record.timeTo.toDate()) ?? "";
-    var yesCount = (record.yesCount == null) ? "0" : record.yesCount.toString();
-    var noCount = (record.noCount == null) ? "0" : record.noCount.toString();
-    var maybeCount =
-        (record.maybeCount == null) ? "0" : record.maybeCount.toString();
-    var waitlistCount = (record.waitlistCount == null)
-        ? ""
-        : record.waitlistCount.toString() + " Waitlist |";
-    var cap1Name = record.captain1Name ?? "";
-    var cap2Name = record.captain2Name ?? "";
-    var locationStr = record.location ?? "";
-    final List<String> entries = <String>['A', 'B', 'C','A', 'B', 'C','A', 'B', 'C','A', 'B', 'C'];
-    final List<int> colorCodes = <int>[600, 500, 100,600, 500, 100,600, 500, 100,600, 500, 100];
-    return new Column(children: <Widget>[
-      new ListTile(
-          leading: const Icon(Icons.access_time, color: Colors.indigoAccent),
-          title: Text(toDateStr),
-          subtitle: Text(timeStr)),
-      new ListTile(
-          leading: const Icon(Icons.location_on, color: Colors.indigoAccent),
-          title: Text(locationStr)),
-      new ListTile(
-        leading: const Icon(Icons.thumbs_up_down, color: Colors.indigoAccent),
-        title: Text("Yes : " + yesCount),
-        subtitle: Text("No : " + noCount + " | Maybe : " + maybeCount),
-      ),
-      new ListTile(
-          leading: const Icon(Icons.people_outline, color: Colors.indigoAccent),
-          title: Text(cap1Name + " versus " + cap2Name)),
-      new Container(
-        width: MediaQuery.of(context).size.width * 0.9,
-        height: MediaQuery.of(context).size.height * 0.9,
-          // new Expanded(
-          child: ListView.builder(
-              padding: const EdgeInsets.all(8.0),
-              itemCount: entries.length,
-              itemBuilder: (BuildContext context, int index) {
-                return Container(    
-                  height: 51,
-                  color: Colors.amber[colorCodes[index]],
-                  child: Center(child: Text('Entry ${entries[index]}')),
-                );
-              })),
-    ]);
-    // ]);
-  }
-
   Widget _buildAddGame(BuildContext _context) {
     TextEditingController fromDateController = TextEditingController();
     TextEditingController toDateController = TextEditingController();
@@ -221,7 +163,6 @@ class TeamListState extends State<TeamList> {
               child: OutlineButton(
                 highlightColor: Colors.amber,
                 onPressed: () async {
-                  print(_addGameFormKey.currentState);
                   _addGameFormKey.currentState.save();
                   DateFormat df = new DateFormat("EEE, MMM yyyy dd h:mma");
                   var fromDate = (fromDateController.text == "")
@@ -364,6 +305,133 @@ class TeamListState extends State<TeamList> {
             ),
           ]),
         ));
+  }
+
+  Widget _viewGame(BuildContext context, GameModel record) {
+    var timeStr = " ";
+    var toDateStr;
+    if (record.timeFrom != null) {
+      toDateStr =
+          new DateFormat.yMMMMEEEEd("en_US").format(record.timeFrom.toDate());
+      var fromTimeStr =
+          new DateFormat.Hm("en_US").format(record.timeFrom.toDate());
+      var toTimeStr = new DateFormat.Hm("en_US").format(record.timeTo.toDate());
+      timeStr = "From " + fromTimeStr + " to " + toTimeStr;
+    }
+    // var fromDateStr = new DateFormat.yMMMMEEEEd("en_US").format(record.timeTo.toDate()) ?? "";
+    var yesCount = (record.yesCount == null) ? "0" : record.yesCount.toString();
+    var noCount = (record.noCount == null) ? "0" : record.noCount.toString();
+    var maybeCount =
+        (record.maybeCount == null) ? "0" : record.maybeCount.toString();
+    var waitlistCount = (record.waitlistCount == null)
+        ? ""
+        : record.waitlistCount.toString() + " Waitlist |";
+    var cap1Name = record.captain1Name ?? "";
+    var cap2Name = record.captain2Name ?? "";
+    var locationStr = record.location ?? "";
+
+    return new SingleChildScrollView(
+        padding: const EdgeInsets.all(8.0),
+        child: new Column(children: <Widget>[
+          new ListTile(
+              leading:
+                  const Icon(Icons.access_time, color: Colors.indigoAccent),
+              title: Text(toDateStr),
+              subtitle: Text(timeStr)),
+          new ListTile(
+              leading:
+                  const Icon(Icons.location_on, color: Colors.indigoAccent),
+              title: Text(locationStr)),
+          new ListTile(
+            leading:
+                const Icon(Icons.thumbs_up_down, color: Colors.indigoAccent),
+            title: Text("Yes : " + yesCount),
+            subtitle: Text("No : " + noCount + " | Maybe : " + maybeCount),
+          ),
+          new ListTile(
+              leading:
+                  const Icon(Icons.people_outline, color: Colors.indigoAccent),
+              title: Text(cap1Name + " versus " + cap2Name)),
+          new PlayerList(game: record, status: "No"),
+          //  new PlayerList(game: record, status: "No"),
+        ]));
+    // ]);
+  }
+}
+
+class PlayerList extends StatefulWidget {
+  const PlayerList(
+      {Key key,
+      @required this.game,
+      @required this.status})
+      : super(key: key);
+
+  final GameModel game;
+  final String status;
+  @override
+  PlayerListState createState() =>
+      new PlayerListState(game, status);
+}
+
+class PlayerListState extends State<PlayerList>{
+  PlayerListState(this.game, this.status);
+  final String status;
+  final GameModel game;
+  final Firestore fireStore = Firestore.instance;
+  @override
+  Widget build(BuildContext context) {
+    Firestore fireStore = Firestore.instance;
+    List<DocumentSnapshot> snapshotList;
+    return StreamBuilder<QuerySnapshot>(
+      stream: fireStore
+          .collection('GamePlayers')
+          .where("Game", isEqualTo: game.reference)
+          // .where("YesNoMaybe", isEqualTo: this.status)
+          .orderBy("YesNoMaybe",descending: true)
+          .snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) return LinearProgressIndicator();
+        snapshotList = snapshot.data.documents;
+        Icon _bIcon = (this.status == 'Yes')
+            ? Icon(Icons.check_circle, color: Colors.green)
+            : Icon(Icons.cancel, color: Colors.red);
+        return  new Container(
+                width: MediaQuery.of(context).size.width * 0.6,
+                height: MediaQuery.of(context).size.height * 0.8,
+                child: ListView.builder(
+                    itemCount: snapshotList.length,
+                    padding: const EdgeInsets.all(0.0),
+                    itemBuilder: (BuildContext context, int index) {
+                      
+                      var record = snapshotList[index].data;
+                      var _bColor = Colors.green[50];
+                      var _sYesNo = record["YesNoMaybe"];
+                      if (_sYesNo == "No") {
+                        _bColor = Colors.deepOrange[50];
+                        _bIcon = Icon(Icons.thumb_down, color: Colors.deepOrange);
+                      } else if (_sYesNo == "Maybe") {
+                        _bColor = Colors.deepPurple[50];
+                        _bIcon = Icon(Icons.all_inclusive,color: Colors.purple);
+                      }else {
+                        _bColor = Colors.green[50];
+                        _bIcon = Icon(Icons.thumb_up, color: Colors.green);
+                      }
+                      return Container(
+                          color: _bColor,
+                          child: ListTile(
+                            leading:  _bIcon,    
+                            title: Text(
+                              record["PlayerName"],
+                              style: TextStyle(
+                                  fontSize: 16.0,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.blueGrey),
+                            ),
+                            dense: true,
+                          ));
+                    }));
+      },
+    );
   }
 }
 
@@ -575,7 +643,8 @@ class GameModel extends Model {
       "NoCount": 0,
       "MaybeCount": 0,
       "WaitlistCount": 0,
-      "GuestCount": 0
+      "GuestCount": 0,
+      "Reference": reference
     };
   }
 
